35b4535dab93c4bdd83605a072bd7340e43eafa4
George Dietrich <george@dietrich.app>
2022-08-29 00:36:08 -0400

    Implement fairly decent way of tracking gnucash settings

--- aconfmgr/40-users.sh
+++ aconfmgr/40-users.sh
@@ -14,6 +14,9 @@ AddPackage foot # Fast, lightweight, and minimalistic Wayland terminal emulator
 CopyFile /etc/shells
 CopyFile /etc/inputrc
 
+# Utilities
+AddPackage unzip # open .zip
+
 # Common user binaries
 CopyFile /usr/local/bin/tax-calc 755
 CopyFile /usr/local/bin/uuid 755
