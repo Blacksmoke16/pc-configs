aec64759ac6dbe355af4d30a3903f257b820a7c9
George Dietrich <george@dietrich.app>
2022-09-20 19:16:15 -0400

    Setup printing
    Switch to open source nvidia drivers

--- aconfmgr/50-desktop.sh
+++ aconfmgr/50-desktop.sh
@@ -1,5 +1,5 @@
 # GPU
-AddPackage nvidia # NVIDIA drivers for linux
+AddPackage nvidia-open # NVIDIA open kernel modules
 
 # Sway & friends
 AddPackage mako # Lightweight notification daemon for Wayland
@@ -23,6 +23,19 @@ AddPackage wl-clipboard # Command-line copy/paste utilities for Wayland
 # Core
 AddPackage discord # All-in-one voice and text chat for gamers that's free and secure.
 AddPackage --foreign google-chrome # The popular and trusted web browser by Google (Stable Channel)
+
+# Printing
+AddPackage cups # The CUPS Printing System - daemon package
+AddPackage --foreign brother-mfc-j885dw # LPR and CUPS driver for the Brother MFC-J885DW printer
+CreateLink /etc/systemd/system/sockets.target.wants/cups.socket /usr/lib/systemd/system/cups.socket
+CopyFile /etc/cups/ppd/color.ppd 640 '' cups
+function PrinterConfFilter() {
+    sed 's/Attribute marker-change-time .*/Attribute marker-change-time 1663713996/' | \
+        sed 's/StateTime .*/StateTime 1663713996/' | \
+        sed 's/Attribute marker-levels .*/Attribute marker-levels -1,-1,-1,-1,-1/'
+}
+AddFileContentFilter '/etc/cups/printers.conf' PrinterConfFilter
+CopyFile /etc/cups/printers.conf 600
 
 # Gnucash
 AddPackage gnucash # Personal and small-business financial-accounting application
