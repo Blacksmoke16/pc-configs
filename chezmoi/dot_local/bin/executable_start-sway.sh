cd86271b0dddd5d8fea332387d1c0c1ddacc6bf5
George Dietrich <dev@dietrich.pub>
2024-01-31 22:03:15 -0500

    Save tweaks

--- chezmoi/dot_local/bin/executable_start-sway.sh
+++ chezmoi/dot_local/bin/executable_start-sway.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 # Set WLRoots renderer to Vulkan to avoid flickering
-# export WLR_RENDERER=vulkan
+export WLR_RENDERER=vulkan
 
 # General wayland environment variables
 export XDG_SESSION_TYPE=wayland
 export QT_QPA_PLATFORM=wayland
 export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
-# export SDL_VIDEODRIVER=wayland
+export SDL_VIDEODRIVER=wayland
 
 # Xwayland compatibility
 export XWAYLAND_NO_GLAMOR=1
