771eb913f881b304dca4d930c2ee7b33320d723c
George Dietrich <dev@dietrich.pub>
2024-07-02 14:39:52 -0400

    De-couple from work for now Remove Typora

--- aconfmgr/60-development.sh
+++ aconfmgr/60-development.sh
@@ -2,7 +2,6 @@
 AddPackage sublime-text # Sublime Text is a sophisticated text editor for code, markup and prose
 AddPackage terraform # HashiCorp tool for building and updating infrastructure as code idempotently
 AddPackage watchexec # Executes commands in response to file modifications
-AddPackage --foreign typora # A minimal markdown editor and reader.
 AddPackage --foreign jetbrains-toolbox # Manage all your JetBrains Projects and Tools
 AddPackage --foreign postman-bin # Build, test, and document your APIs faster
 AddPackage --foreign xxd-standalone # Hexdump utility from vim
