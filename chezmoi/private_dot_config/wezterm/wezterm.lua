9923ea8d2cfbff1f02cee864714e3f958d9a91f5
George Dietrich <dev@dietrich.pub>
2024-07-03 11:10:55 -0400

    Start wezterm with split view Remove unused desktop applications

--- chezmoi/private_dot_config/wezterm/wezterm.lua
+++ chezmoi/private_dot_config/wezterm/wezterm.lua
@@ -1,9 +1,7 @@
 local wezterm = require 'wezterm'
 
 config = wezterm.config_builder()
-
 config.scrollback_lines = 10000
-
 config.enable_scroll_bar = true
 
 config.font = wezterm.font 'Roboto Mono'
@@ -13,5 +11,10 @@ config.colors = {
   background = '#222222',
   scrollbar_thumb = '#b9b9b9',
 }
+
+wezterm.on('gui-startup', function(cmd)
+  local tab, pane, window = wezterm.mux.spawn_window(cmd or {})
+  pane:split { size = 0.5 }
+end)
 
 return config
