4c22e66071beb44f004d050cc117fc2563520c68
George Dietrich <dev@dietrich.pub>
2025-03-21 12:37:33 -0400

    Switch from `SublimeTest` to `Zed` Some additional tweaks

--- aconfmgr/60-development.sh
+++ aconfmgr/60-development.sh
@@ -1,6 +1,5 @@
 # General
 AddPackage just # A handy way to save and run project-specific commands
-AddPackage sublime-text # Sublime Text is a sophisticated text editor for code, markup and prose
 AddPackage terraform # HashiCorp tool for building and updating infrastructure as code idempotently
 AddPackage watchexec # Executes commands in response to file modifications
 AddPackage --foreign jetbrains-toolbox # Manage all your JetBrains Projects and Tools
@@ -8,10 +7,13 @@ AddPackage --foreign postman-bin # Build, test, and document your APIs faster
 AddPackage --foreign xxd-standalone # Hexdump utility from vim
 AddPackage bind # A complete, highly portable implementation of the DNS protocol
 AddPackage less # A terminal based program for viewing text files
+AddPackage rsync # A fast and versatile file copying tool for remote and local files (required for zed auto-update)
+AddPackage --foreign shellcheck-bin # Shell script analysis tool (binary release, static)
 
 # Docker
 AddPackage docker # Pack, ship and run any application as a lightweight container
 AddPackage docker-compose # Fast, isolated development environments using Docker
+AddPackage docker-buildx # Docker CLI plugin for extended build capabilities with BuildKit
 CreateLink /etc/systemd/system/multi-user.target.wants/docker.service /usr/lib/systemd/system/docker.service
 
 # Build dependencies
